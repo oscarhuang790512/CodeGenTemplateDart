a9cc42416a8e29c0d42d2df9d1a7b1d64128641f
OscarHuang <oscar.huang@titansoft.com.sg>
2024-07-04 23:02:20 +0800

    increase Compatibility Range on 1.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.example"
-version = "1.0.4"
+version = "1.0.5"
 
 repositories {
     mavenCentral()
@@ -28,7 +28,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("213")
-        untilBuild.set("234.*")
+        untilBuild.set("242.*")
     }
 
     signPlugin {
